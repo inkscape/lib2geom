384aa7397a49f9d8f97832bc35e1f16640699059
Aaron Spike <aaron@ekips.org>
2006-09-17 18:18:33 +0000

    meshing troubles

--- gear.cpp
+++ gear.cpp
@@ -187,7 +187,7 @@ Gear Gear::spawn(int N, double a) {
     if (gear.number_of_teeth() % 2 == 0)
         new_angle -= gear.tooth_thickness_angle();
     new_angle -= (_angle) * (pitch_radius() / gear.pitch_radius());
-    //new_angle += (a) * (pitch_radius() / gear.pitch_radius());
+    new_angle += (a) * (pitch_radius() / gear.pitch_radius());
     gear.angle(new_angle);
     return gear;
 };
