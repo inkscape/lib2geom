1180128175d836ac16dbc781bc05a67a598211a8
Aaron Spike <aaron@ekips.org>
2006-09-26 01:21:13 +0000

    change the path separator in mingwenv.bat

--- mingwenv.bat
+++ mingwenv.bat
@@ -2,5 +2,5 @@
 set MINGW_BIN=c:\mingw\bin
 set CMAKE_BIN=c:\cmake\bin
 set GTK_BIN=c:\gtk28\bin
-set PKG_CONFIG_PATH=c:/gtk28/lib/pkgconfig
+set PKG_CONFIG_PATH=c:\gtk28\lib\pkgconfig
 set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%PATH%
