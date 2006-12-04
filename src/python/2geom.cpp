6c25e2655492f65ff2f78e263c2ed1478e30bb37
Aaron Spike <aaron@ekips.org>
2006-12-04 13:14:47 +0000

    avoid namespace collision; clean cmake debugs

--- src/python/2geom.cpp
+++ src/python/2geom.cpp
@@ -6,6 +6,6 @@ using namespace boost::python;
 BOOST_PYTHON_MODULE(lib2geom_py)
 {
     class_<Geom::Point>("Point", init<double, double>())
-//        .def(str(self))
+        .def(self_ns::str(self))
     ;
 }
