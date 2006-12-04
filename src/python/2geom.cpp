019d2d0a37ecdbd4f1b473a4ef6afd638e2fa155
Aaron Spike <aaron@ekips.org>
2006-12-04 18:25:57 +0000

    add a few operators to Point() wrapper

--- src/python/2geom.cpp
+++ src/python/2geom.cpp
@@ -7,5 +7,20 @@ BOOST_PYTHON_MODULE(lib2geom_py)
 {
     class_<Geom::Point>("Point", init<double, double>())
         .def(self_ns::str(self))
+
+	.def("polar", &Geom::Point::polar)
+	.staticmethod("polar")
+
+	.def("ccw", &Geom::Point::ccw)
+	.def("cw", &Geom::Point::cw)
+	.def("round", &Geom::Point::round)
+	.def("normalize", &Geom::Point::normalize)
+
+	.def(self += self)
+	.def(self -= self)
+	.def(self /= float())
+	.def(self *= float())
+
+	.def(self == self)
     ;
 }
