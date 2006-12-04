44e2d12e02bdc5a8b267765e296542729f018848
Aaron Spike <aaron@ekips.org>
2006-12-04 17:04:24 +0000

    I'm moving to gtk2.10

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,6 +1,6 @@
 @echo Setting environment variables for MinGw build of lib2geom
 set MINGW_BIN=c:\mingw\bin
 set CMAKE_BIN=c:\cmake\bin
-set GTK_BIN=c:\gtk28\bin
-set PKG_CONFIG_PATH=c:\gtk28\lib\pkgconfig
+set GTK_BIN=c:\gtk210\bin
+set PKG_CONFIG_PATH=c:\gtk210\lib\pkgconfig
 set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%PATH%
