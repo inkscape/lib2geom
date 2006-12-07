01d75226ceeafe34ce4aacd3003c16597f51a200
Aaron Spike <aaron@ekips.org>
2006-12-07 05:01:26 +0000

    start binding Matrix

--- src/python/2geom.cpp
+++ src/python/2geom.cpp
@@ -1,6 +1,7 @@
 #include "point.h"
 #include "point-ops.h"
 #include "point-fns.h"
+#include "matrix.h"
 
 #include <boost/python.hpp>
 #include <boost/python/implicit.hpp>
@@ -93,6 +94,23 @@ BOOST_PYTHON_MODULE(lib2geom_py)
 // TODO: explain why this gives a compile time error
 //    implicitly_convertible<tuple,Geom::Point>();
 
+    class_<Geom::Matrix>("Matrix", init<double, double, double, double, double, double>())
+        .def(self_ns::str(self))
+        .add_property("x_axis",&Geom::Matrix::x_axis,&Geom::Matrix::set_x_axis)
+        .add_property("y_axis",&Geom::Matrix::y_axis,&Geom::Matrix::set_y_axis)
+        .add_property("translation",&Geom::Matrix::translation,&Geom::Matrix::set_translation)
+        .def("is_translation", &Geom::Matrix::is_translation)
+        .def("is_rotation", &Geom::Matrix::is_rotation)
+        .def("is_scale", &Geom::Matrix::is_scale)
+        .def("is_uniform_scale", &Geom::Matrix::is_uniform_scale)
+        .def("set_identity", &Geom::Matrix::set_identity)
+        .def("det", &Geom::Matrix::det)
+        .def("descrim2", &Geom::Matrix::descrim2)
+        .def("descrim", &Geom::Matrix::descrim)
+        .def("expansion", &Geom::Matrix::expansion)
+        .def("expansionX", &Geom::Matrix::expansionX)
+        .def("expansionY", &Geom::Matrix::expansionY)
+    ;
 }
 
 /*
