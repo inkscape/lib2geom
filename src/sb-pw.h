d50aa23c6417e8f7fbfe5cfdef7840ab875243b6
Michael G. Sloan <mgsloan@gmail.com>
2007-02-05 03:18:34 +0000

    Improved partition function

--- src/sb-pw.h
+++ src/sb-pw.h
@@ -14,35 +14,51 @@ class pw_sb {
     //segs[i] stretches from cuts[i] to cuts[i+1]. 
 
     pw_sb partition(vector<double> const &c) {
-        ret = pw_sb(this);
-        //Segment index, Cut index
+        pw_sb ret = pw_sb();
+        //just a bit of optimizing reservation
+        ret.cuts.reserve(c.size() + cuts.size());
+        ret.segs.reserve(c.size() + cuts.size() - 1);
+ 
+       //Segment index, Cut index
         int si = 0, ci = 0;
+
         //if the input cuts have something earlier than this pw_sb, add sections of zero
-        while(c[ci] < cuts[si] && ci < c.size()) {
-            ret.segs.insert(segs.begin()+si, BezOrd(0, 0));
-            ret.cuts.insert(cuts.begin()+si, c[ci]);
-            si++; ci++;
+        while(ci < c.size() && c[ci] < cuts[0]) {
+            ret.cuts.push_back(c[ci]);
+            prev = c[ci];
+            ret.segs.push_back(SBasis());
+            ci++;
         }
+        ret.cuts.push_back(cuts[0]);
+        double prev = cuts[0];
         while(si < segs.size() && ci < c.size()) {
-            if(c[ci] > cuts[si]) {  //no more cuts within this segment; move-along
+            if(c[ci] >= cuts[si + 1]) {  //no more cuts within this segment
+                if(prev > cuts[si]) {
+                    ret.segs.push_back(segs[si].portion( (prev - cuts[si]) / (cuts[si+1] - cuts[si]), 1);
+                } else {
+                    ret.segs.push_back(segs[si]);
+                }
                 si++;
+                ret.cuts.push_back(cuts[si]);
+                prev = cuts[si];
+            } else if(c[ci] == cuts[si]) { //coincident
+                //Already finalized the seg with the code immediately above
+                ci++;
             } else {
-                SBasis a, b;
-                ret.segs[si].split( (c[ci] - ret.cuts[si]) / (ret.cuts[si+1] - ret.cuts[si]), a, b);
-                ret.segs.remove(ret.segs.begin() + si);
-                ret.segs.insert(ret.segs.begin() + si, a);
-                si++;
-                ret.segs.insert(ret.segs.begin() + si, b);
-                ret.cuts.insert(ret.cuts.begin() + si, c[ci]);
+                double rwidth = 1 / (cuts[si+1] - cuts[si]);
+                ret.cuts.push_back(c[ci]);
+                prev = c[ci];
+                ret.segs.push_back(segs[si].portion( (prev - cuts[si]) * rwidth, (c[ci] - cuts[si]) * rwidth ));
                 ci++;
             }
         }
         //if the input cuts extend further than this pw_sb, add sections of zero
         while(ci < c.size()) {
-            ret.segs.push_back(BezOrd(0, 0));
             ret.cuts.push_back(c[ci]);
+            ret.segs.push_back(SBasis());
             ci++;
         }
+        return ret;
     }
 };
 
@@ -82,6 +98,16 @@ class pw_piece {
     pw_piece(SBasis s, double st, double en) : seg(s), start(st), end(en) {}
 }
 */
+
+    /*splitSeg(int i, double t) {
+        SBasis a, b;
+        segs[i].split((t-cuts[i]) / (cuts[i+1]-cuts[i]), a, b);
+        vector<SBasis>::iterator it = segs.begin() + i
+        segs.remove(it);
+        segs.insert(it, a);
+        segs.insert(it+1, b);
+        cuts.insert(cuts.begin() + i + 1, c[ci]);
+    }*/
 
 }
 
