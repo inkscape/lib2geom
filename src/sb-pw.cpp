1480e70bebfa6fd8f82f1d63d1f7425f4d29ad83
Michael G. Sloan <mgsloan@gmail.com>
2007-02-09 04:25:59 +0000

    sb-pw portion function

--- src/sb-pw.cpp
+++ src/sb-pw.cpp
@@ -1,6 +1,11 @@
 #include "sb-pw.h"
 
 namespace Geom {
+SBasis elem_portion(const pw_sb &a, int i, double from, double to) {
+    double rwidth = 1 / (a.cuts[i+1] - a.cuts[i]);
+    return portion( a.segs[i], (from - a.cuts[i]) * rwidth, (to - a.cuts[i]) * rwidth );
+}
+
 pw_sb partition(const pw_sb &t, vector<double> const &c) {
     pw_sb ret = pw_sb();
     //just a bit of optimizing reservation
@@ -20,7 +25,7 @@ pw_sb partition(const pw_sb &t, vector<double> const &c) {
     while(si < t.segs.size() && ci < c.size()) {
         if(c[ci] >= t.cuts[si + 1]) {  //no more cuts within this segment
             if(prev > t.cuts[si]) {
-                ret.segs.push_back(portion(t.segs[si], (prev - t.cuts[si]) / (t.cuts[si+1] - t.cuts[si]), 1));
+                ret.segs.push_back(portion(t.segs[si], (prev - t.cuts[si]) / (t.cuts[si+1] - t.cuts[si]), 1.0));
             } else {
                 ret.segs.push_back(t.segs[si]);
             }
@@ -31,10 +36,9 @@ pw_sb partition(const pw_sb &t, vector<double> const &c) {
             //Already finalized the seg with the code immediately above
             ci++;
         } else {
-            double rwidth = 1 / (t.cuts[si+1] - t.cuts[si]);
+	    ret.segs.push_back(elem_portion(t, si, prev, c[ci]));
             ret.cuts.push_back(c[ci]);
             prev = c[ci];
-            ret.segs.push_back(portion(t.segs[si], (prev - t.cuts[si]) * rwidth, (c[ci] - t.cuts[si]) * rwidth ));
             ci++;
         }
     }
@@ -46,6 +50,47 @@ pw_sb partition(const pw_sb &t, vector<double> const &c) {
     }
     return ret;
 }
+
+pw_sb portion(const pw_sb &a, double from, double to) {
+    int fi = 0, ti = 0; //from/to indexes
+    for(int i = 0; i < a.segs.size(); i++) {
+	if(a.cuts[i] < from && from < a.cuts[i+1]) fi = i; 
+	if(a.cuts[i] < to && to < a.cuts[i+1]) ti = i;
+    }
+    if (fi > a.cuts[a.segs.size()]) fi = a.segs.size() - 1;
+    if (ti > a.cuts[a.segs.size()]) ti = a.segs.size() - 1;
+
+    pw_sb ret = pw_sb();
+    
+    if (fi < ti) {
+	ret.segs.reserve(ti - fi + 1);
+	ret.cuts.reserve(ti - fi + 2);
+	//TODO: consider coincidince
+	ret.cuts.push_back(from);
+	ret.segs.push_back( portion(a.segs[fi], (from - a.cuts[fi]) / (a.cuts[fi+1] - a.cuts[fi]), 1.0) );
+        for(int i = fi + 1; i < ti; i++) {
+	    ret.cuts.push_back(a.cuts[i]);
+	    ret.segs.push_back(a.segs[i]);
+        }
+	ret.cuts.push_back(a.cuts[ti]);
+	ret.segs.push_back( portion(a.segs[ti], 0.0, (to - a.cuts[ti]) / (a.cuts[ti+1] - a.cuts[ti])) );
+	ret.cuts.push_back(a.cuts[ti + 1]);
+    } else if (fi > ti) {
+	ret.segs.reserve(fi - ti + 1);
+	ret.cuts.reserve(fi - ti + 2);
+	//TODO: use above as model.  Use SBasis reverse(SBasis) function
+        for(int i = fi - 1; i < ti; i--) {
+	    
+        }
+    } else {
+	ret.segs.reserve(1);
+	ret.cuts.reserve(2);
+        ret.cuts.push_back(from);
+	ret.segs.push_back(elem_portion(a, fi, from, to));
+	ret.cuts.push_back(to);
+    }
+}
+
 
 /* This macro provides a mapping of operations on pw_sb pieces
  * It assumes that it is the contents of a function, and there is a pw_sb parameter, a.
@@ -116,8 +161,23 @@ struct sbasis_div{static SBasis op(SBasis const &a, SBasis const &b) {return div
 pw_sb divide(pw_sb const &a, pw_sb const &b, int k) { ZipSBWith<sbasis_div<k> >(a, b);}
 */
 
+/* General Plan:
+ * First, the range of b must be found.  One method for this might be roots of
+ * the derivative in order to find extrema.  There's no need to worry about
+ * the second derivative, as even in the cases of f''=0, it will just be an
+ * intermediate value on the way to an actual extrema.  In the case of
+ * consistant runs of f'=0, one representative point should be selected.
+ * 
+ * At this point, the domain of a which is outside the range of b may be ignored.
+ * 
+ * The roots of the derivative are once again used to find points of 'loopback'
+ * where a is retraversed, in the opposite direction.
+ *
+ * Hmm, at this point its a bit fuzzy - compositions and manipulations on
+ * individual sbasis.
+ */
 pw_sb compose(pw_sb const &a, pw_sb const &b) {
-
+    
 }
 
 /*
