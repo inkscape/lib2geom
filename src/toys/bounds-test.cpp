50ec4f94b84d780ffeb115b47b8db591dbcf26dd
JF Barraud <jf.barraud@gmail.com>
2007-02-25 22:12:46 +0000

    testing a local_bound function, and a root finder... Still a draft version!! Needs some more bug fixing.

--- src/toys/bounds-test.cpp
+++ src/toys/bounds-test.cpp
@@ -16,185 +16,190 @@ using namespace Geom;
 using namespace std;
 
 static void plot(cairo_t* cr, SBasis const &B,double vscale=1,double a=0,double b=1){
-      MultidimSBasis<2> plot;
-      plot[0]=SBasis(BezOrd(150+a*300,150+b*300));
-      plot[1]=-vscale*B;
-      plot[1]+=300;
-      cairo_md_sb(cr, plot);
-      cairo_stroke(cr);
+  MultidimSBasis<2> plot;
+  plot[0]=SBasis(BezOrd(150+a*300,150+b*300));
+  plot[1]=-vscale*B;
+  plot[1]+=300;
+  cairo_md_sb(cr, plot);
+  cairo_stroke(cr);
+}
+static void plot_bar(cairo_t* cr, double height, double vscale=1,double a=0,double b=1){
+  cairo_move_to(cr, Geom::Point(150+300*a,-height*vscale+300));
+  cairo_line_to(cr, Geom::Point(150+300*b,-height*vscale+300));
+  cairo_stroke(cr);
+}
+static void plot_box(cairo_t* cr, double a, double b, double min, double max){
+  cairo_rectangle (cr,150+a*300,-max+300,300*(b-a),max-min);
+  cairo_fill (cr);
 }
 
-static double upper_bound(SBasis f, unsigned NbCuts=3){
-  f.normalize();
-  if (f.size()==0){return(0);}
-  if (f.size()==1){return(max(f[0][0],f[0][1]));}
-  //  if (f.size()==2){return(max_BB(f[0],f[1]));}
-
-  double M=f[0][0];
-  BezOrd sb10_hi[3*NbCuts];
-  BezOrd sb10_lo[3*NbCuts];
-  BezOrd sb01_hi[3*NbCuts];
-  BezOrd sb01_lo[3*NbCuts];
-  //linearize s(t)*(1-t)
-  for (int i=0; i<NbCuts; i++){
-    double a,b,t,fa,fb,ft,vt;
-    a= i   /3./NbCuts;
-    b=(i+1)/3./NbCuts;
-    fa=a*(1-a)*(1-a);
-    fb=b*(1-b)*(1-b);
-    sb10_lo[i]=BezOrd(fa-(fb-fa)/(b-a)*a,fb+(fb-fa)/(b-a)*(1-b));
-    t=(a+b)/2;
-    ft=t*(1-t)*(1-t);
-    vt=(1-t)*(1-3*t);
-    sb10_hi[i]=BezOrd(ft-vt*t,ft+vt*(1-t));
-
-    a+=1./3.;
-    b+=1./3.;
-    fa=a*(1-a)*(1-a);
-    fb=b*(1-b)*(1-b);
-    sb10_lo[i+NbCuts]=BezOrd(fa-(fb-fa)/(b-a)*a,fb+(fb-fa)/(b-a)*(1-b));
-    t=(a+b)/2;
-    ft=t*(1-t)*(1-t);
-    vt=(1-t)*(1-3*t);
-    sb10_hi[i+NbCuts]=BezOrd(ft-vt*t,ft+vt*(1-t));
-
-    a+=1./3.;
-    b+=1./3.;
-    fa=a*(1-a)*(1-a);
-    fb=b*(1-b)*(1-b);
-    sb10_hi[i+2*NbCuts]=BezOrd(fa-(fb-fa)/(b-a)*a,fb+(fb-fa)/(b-a)*(1-b));
-    t=(a+b)/2;
-    ft=t*(1-t)*(1-t);
-    vt=(1-t)*(1-3*t);
-    sb10_lo[i+2*NbCuts]=BezOrd(ft-vt*t,ft+vt*(1-t));
-  }
-
-  //linearize s(t)*t...
-  for (int i=0; i<3*NbCuts; i++){
-    double x;
-    sb01_lo[i]=sb10_lo[3*NbCuts-i-1];
-    x=sb01_lo[i][0];sb01_lo[i][0]=sb01_lo[i][1];sb01_lo[i][1]=x;
-    sb01_hi[i]=sb10_hi[3*NbCuts-i-1];
-    x=sb01_hi[i][0];sb01_hi[i][0]=sb01_hi[i][1];sb01_hi[i][1]=x;
-  }
-
-  BezOrd tail[3*NbCuts];
-  BezOrd new_tail[3*NbCuts];
-  int deg_max=f.size()-1;
-  for (int i=0; i<3*NbCuts; i++){
-    tail[i]=f[deg_max];
-  }
-  for (int deg=deg_max;deg>0;deg--){
-    for (int i=0; i<3*NbCuts; i++){
-      if (tail[i][0]>0){
-	new_tail[i]=f[deg-1]+tail[i][0]*sb10_hi[i];
-      }else{
-	new_tail[i]=f[deg-1]+tail[i][0]*sb10_lo[i];
-      }
-      if (tail[i][1]>0){
-	new_tail[i]+=tail[i][1]*sb01_hi[i];
-      }else{
-	new_tail[i]+=tail[i][1]*sb01_lo[i];
-      }
-      tail[i]=new_tail[i];
+static void local_bounds(SBasis const & s,
+		  double t0,double t1,
+		  double &lo, double &hi) {
+  int imax=s.size()-1;
+  lo=0;
+  hi=0;
+  
+  for(int i = imax; i >=0; i--) {
+    double a=s[i][0];
+    double b=s[i][1];
+    double t;
+    if (hi>0){t=((b-a)+hi)/2/hi;}
+    if (hi<=0||t<t0||t>t1){
+      hi=std::max(a*(1-t0)+b*t0+hi*t0*(1-t0),a*(1-t1)+b*t1+hi*t1*(1-t1));
+    }else{
+      hi=a*(1-t)+b*t+hi*t*(1-t);	  
+    }
+    if (lo<0){t=((b-a)+lo)/2/lo;}
+    if (lo>=0||t<t0||t>t1){
+      lo=std::min(a*(1-t0)+b*t0+lo*t0*(1-t0),a*(1-t1)+b*t1+lo*t1*(1-t1));
+    }else{
+      lo=a*(1-t)+b*t+lo*t*(1-t);	  
     }
   }
-  for (int i=0; i<3*NbCuts; i++){
-    double a,b;
-    a= i   /3./NbCuts;
-    b=(i+1)/3./NbCuts;
-    M=max(M,max(tail[i](a),tail[i](b)));
-  }
-  return(M);
 }
 
-static double cut_bounds(SBasis f,double &m, double &M, unsigned NbCuts=3){
-  M= upper_bound( f,NbCuts);
-  m=-upper_bound(-f,NbCuts);
+static void my_roots_internal(SBasis const &f,
+			      SBasis const &df,
+			      vector<double> &roots,
+			      double tol,
+			      double a,
+			      double fa,
+			      double b,
+			      double fb){
+
+  if (f.size()==0){
+    roots.push_back(a);
+    roots.push_back(b);
+    return;
+  }
+  if (f.size()==1){
+    if(f[0][0]==0&&f[0][1]==0){
+      roots.push_back(a);
+      roots.push_back(b);
+    }else{
+      if (fa*fb<=0) roots.push_back(a-fa/(fb-fa)*(b-a));
+    }
+    return;
+  }
+  
+  if ((b-a)<tol){
+    //TODO: use different tol for t and f !
+    if(fa*fb<=0||fabs(fa)<tol||fabs(fb)<tol){
+      roots.push_back((a+b)/2);
+    }
+    return;
+  }
+
+  double m,M;
+  local_bounds(df,a,b,m,M);
+  if (m>=0&&(fa> tol||fb<-tol)) return;
+  if (M<=0&&(fa<-tol||fb>tol)) return;
+  
+  double t0,t1,t,ft;
+  t0=(fa<0)?a-fa/M:a-fa/m;
+  t1=(fb>0)?b-fb/M:b-fb/m;
+  if (t0>t1) return;
+  t=(t0+t1)/2;
+  ft=f(t);
+  my_roots_internal(f,df,roots,tol,t0,f(t0),t ,ft   );
+  my_roots_internal(f,df,roots,tol,t ,ft   ,t1,f(t1));
+}
+
+static void new_roots(SBasis const &f,
+		      vector<double> &roots,
+		      double tol=1e-7,
+		      double a=0,
+		      double b=1){
+  SBasis df=derivative(f);
+  my_roots_internal(f,df,roots,tol,a,f(a),b,f(b));
 }
 
 class BoundsTester: public Toy {
+  int size;
 
-    void draw(cairo_t *cr, std::ostringstream *notify, int width, int height, bool save) {
+  void draw(cairo_t *cr, std::ostringstream *notify, int width, int height, bool save) {
     
-      int deg=4;
-	SBasis B;
-	for (int i=0;i<deg;i++){
-	  handles[i    ][0]=150+20*(i-deg);
-	  handles[i+deg][0]=450+20*(i+1);
-	  cairo_move_to(cr, Geom::Point(handles[i    ][0],150));
-	  cairo_line_to(cr, Geom::Point(handles[i    ][0],450));
-	  cairo_move_to(cr, Geom::Point(handles[i+deg][0],150));
-	  cairo_line_to(cr, Geom::Point(handles[i+deg][0],450));
-	}
-	cairo_move_to(cr, Geom::Point(0,300));
-	cairo_line_to(cr, Geom::Point(600,300));
-	
-        cairo_set_line_width (cr, 1);
-        cairo_set_source_rgba (cr, 0.2, 0.2, 0.2, 1);
-        cairo_stroke(cr);
-
-	for (int i=0;i<deg;i++){
-	  B.push_back(BezOrd(-(handles[i][1]-300)*pow(4.,i),-(handles[i+deg][1]-300)*pow(4.,i)));
-	}
-	B.normalize();
-	plot(cr,B,1);	
-        cairo_set_source_rgba (cr, 0., 0., 0.8, 1);
-        cairo_stroke(cr);
-        cairo_set_source_rgba (cr, 0., 0.5, 0., 1);
+    SBasis B;
+    for (int i=0;i<size;i++){
+      handles[i    ][0]=150+15*(i-size);
+      handles[i+size][0]=450+15*(i+1);
+      cairo_move_to(cr, Geom::Point(handles[i    ][0],150));
+      cairo_line_to(cr, Geom::Point(handles[i    ][0],450));
+      cairo_move_to(cr, Geom::Point(handles[i+size][0],150));
+      cairo_line_to(cr, Geom::Point(handles[i+size][0],450));
+    }
+    cairo_move_to(cr, Geom::Point(0,300));
+    cairo_line_to(cr, Geom::Point(600,300));
     
-	double m,M;
-	*notify<<"Use handles to set the coefficients of the s-basis."<<std::endl;
-	*notify<<"-Blue=basic bounds,"<<std::endl;
-	*notify<<"-Red =slower but sharper."<<std::endl;
+    cairo_set_line_width (cr, 1);
+    cairo_set_source_rgba (cr, 0.2, 0.2, 0.2, 1);
+    cairo_stroke(cr);
+    
+    for (int i=0;i<size;i++){
+      B.push_back(BezOrd(-(handles[i     ][1]-300)*pow(4.,i),
+			 -(handles[i+size][1]-300)*pow(4.,i) ));
+    }
+    B.normalize();
+    plot(cr,B,1);	
+    cairo_set_source_rgba (cr, 0., 0., 0.8, 1);
+    cairo_stroke(cr);
+    cairo_set_source_rgba (cr, 0., 0.5, 0., 1);
+    
+    *notify<<"Use handles to set the coefficients of the s-basis."<<std::endl;
+    //*notify<<"-Blue=basic bounds,"<<std::endl;
+    //*notify<<"-Red =slower but sharper?"<<std::endl;
+    
+    double m,M;
+    vector<double>my_roots;
 
-        clock_t end_t;
-        unsigned iterations = 0;
+    cairo_set_source_rgba (cr, 0.9, 0., 0.8, 1);
+    my_roots=roots(B);
+    for(int i=0;i<my_roots.size();i++){
+      draw_handle(cr,Point(150+300*my_roots[i],300));
+    }
+    cairo_set_source_rgba (cr, 0.9, 0., 0.8, 1);
+    my_roots.clear();
+    new_roots(B,my_roots,1e-7,0,1);
+    for(int i=0;i<my_roots.size();i++){
+      draw_cross(cr,Point(150+300*my_roots[i],300));
+    }
+    //cout<<"nb roots: "<<my_roots.size()<<endl;
+    
+    clock_t end_t;
+    unsigned iterations = 0;
 
-        end_t = clock()+clock_t(0.1*CLOCKS_PER_SEC);
-        iterations = 0;
-        while(end_t > clock()) {
-	  bounds(B,m,M);
-	  iterations++;
-        }
-        *notify << 1000*0.1/iterations <<" ms = bounds time"<< std::endl;
-
-	//bounds(B,m,M);
-	cairo_move_to(cr, Geom::Point(150,-m+300));
-	cairo_line_to(cr, Geom::Point(450,-m+300));
-        cairo_set_source_rgba (cr, 0., 0.5, 0.5, 1);
-        cairo_stroke(cr);
-	cairo_move_to(cr, Geom::Point(150,-M+300));
-	cairo_line_to(cr, Geom::Point(450,-M+300));
-        cairo_set_source_rgba (cr, 0., 0.75, 0.75, 1);
-        cairo_stroke(cr);
-
-
-        end_t = clock()+clock_t(0.1*CLOCKS_PER_SEC);
-        iterations = 0;
-        while(end_t > clock()) {
-	  cut_bounds(B,m,M);
-	  iterations++;
-        }
-        *notify << 1000*0.1/iterations <<" ms = cut bounds time"<< std::endl;
-
-	cairo_move_to(cr, Geom::Point(150,-M+300));
-	cairo_line_to(cr, Geom::Point(450,-M+300));
-	cairo_move_to(cr, Geom::Point(150,-m+300));
-	cairo_line_to(cr, Geom::Point(450,-m+300));
-        cairo_set_source_rgba (cr, 0, .75, 0, 1);
-        cairo_stroke(cr);
-
-        Toy::draw(cr, notify, width, height, save);
-    }        
+    my_roots.clear();
+    end_t = clock()+clock_t(0.1*CLOCKS_PER_SEC);
+    iterations = 0;
+    while(end_t > clock()) {
+      my_roots.clear();
+      my_roots=roots(B);
+      iterations++;
+    }
+    *notify << 1000*0.1/iterations <<" ms = roots time"<< std::endl;
+    
+    end_t = clock()+clock_t(0.1*CLOCKS_PER_SEC);
+    iterations = 0;
+    while(end_t > clock()) {
+      my_roots.clear();
+      new_roots(B,my_roots);
+      iterations++;
+    }
+    *notify << 1000*0.1/iterations <<" ms = new_roots time"<< std::endl;
+    
+    Toy::draw(cr, notify, width, height, save);
+  }        
   
 public:
-    BoundsTester(){
-        if(handles.empty()) {
-            for(int i = 0; i < 8; i++)
-	      handles.push_back(Geom::Point(0,300));
-        }
+  BoundsTester(){
+    size=8;
+    if(handles.empty()) {
+      for(int i = 0; i < 2*size; i++)
+	handles.push_back(Geom::Point(0,150+150+uniform()*300*0));
     }
+    handles[0][1]+=10;
+  }
 };
 
 int main(int argc, char **argv) {
@@ -212,5 +217,3 @@ int main(int argc, char **argv) {
   End:
 */
 // vim: filetype = cpp:expandtab:shiftwidth = 4:tabstop = 8:softtabstop = 4:encoding = utf-8:textwidth = 99 :
-
- 	  	 
