887722b6c06e44e6cd52ad73c3a137794f0fc61e
JF Barraud <jf.barraud@gmail.com>
2007-03-04 23:21:16 +0000

    updated compose(pw-sb,sb) to use multi_roots.

--- src/pw-sb.cpp
+++ src/pw-sb.cpp
@@ -216,72 +216,89 @@ pw_sb operator-(pw_sb const &a, pw_sb const &b) { return ZipSBWith(sbasis_sub(),
 pw_sb multiply(pw_sb const &a, pw_sb const &b) { return ZipSBWith(sbasis_mul(), a, b); }
 pw_sb divide(pw_sb const &a, pw_sb const &b, int k) { return ZipSBWith(sbasis_div(k), a, b);}
 
+//--Compose---------------
+/* Quiet ugly atm.
+   A vector<pairs<double,unsigned> > would be better than a map<double,unsigned>.
+   But all this gives me the 'missing ;' blues! --I am not brave enough today-- jfb.
+ */
+static std::map<double,unsigned> vect_of_vect_to_map(std::vector<std::vector<double> >roots ){
+    std::map<double,unsigned> result;
+    for(unsigned i=0; i<roots.size();i++){
+        for(unsigned j=0; j<roots[i].size();j++){
+            result[roots[i][j]]=i;
+        }
+    }
+    return(result);
+}
 
 pw_sb compose(pw_sb const &f, SBasis  const &g){
   pw_sb result;
-  
+
+  if (f.size()==0) return result;
+  if (f.size()==1){
+      double t0 = f.cuts[0], width = f.cuts[1] - t0;
+      return (pw_sb) f.segs[0](compose(BezOrd(-t0 / width, (1-t0) / width), g));
+  }
+
   //first check bounds...
   double M, m;
   bounds(g, m, M);
   if (M < f.cuts.front() || m > f.cuts.back()){
-    int idx = (M < f.cuts.front()) ? 0 : f.cuts.size() - 2;
-    double t0 = f.cuts[idx], width = f.cuts[idx+1] - t0;
-    return (pw_sb) compose(BezOrd(-t0 / width, (1-t0) / width), g);
+      int idx = (M < f.cuts[1]) ? 0 : f.cuts.size()-2;
+      double t0 = f.cuts[idx], width = f.cuts[idx+1] - t0;
+      return (pw_sb) f.segs[idx](compose(BezOrd(-t0 / width, (1-t0) / width), g));
   }
 
-  //-- collect all t / g(t)=f.cuts[idx] for some idx.
+  //-- collect all t / g(t)=f.cuts[idx] for some idx (!= first and last).
   // put them in a map:   t->idx.
-  // Notice that t=0,1 recieve a special treatment:
-  // 0->idx iff f.cuts[idx-1]<g(0)<=f.cuts[idx] (if out of range, f.cut[idx]=+/- infty)
-  // the same for 1.
-  std::map<double,int> cuts_pb; //pb stands for pullback
-  vector<double> sols;
-  for(int i=0; i<f.cuts.size();i++){
-    sols=roots(g-BezOrd(f.cuts[i]));
-    for (vector<double>::iterator root=sols.begin();root!=sols.end();root++)
-      cuts_pb[*root]=i;
-    if((cuts_pb.count(0.)==0) and (g[0][0]<=f.cuts[i]))
+  std::vector<double> levels;//we can forget first and last cuts of f...
+  levels.insert(levels.begin(),f.cuts.begin()+1,f.cuts.end()-1);
+  std::map<double,unsigned> cuts_pb; //pb stands for pullback
+  cuts_pb = vect_of_vect_to_map(multi_roots(g,levels));
+
+  // Also map 0 and 1 to the first level above(or =) f(0) and f(1).
+  if(cuts_pb.count(0.)==0){
+      int i=0;
+      while (i<levels.size()&&(g[0][0]>levels[i])) i++;
       cuts_pb[0.]=i;
-    if((cuts_pb.count(1.)==0) and (g[0][1]<=f.cuts[i]))
+  }
+  if(cuts_pb.count(1.)==0){
+      int i=0;
+      while (i<levels.size()&&(g[0][1]>levels[i])) i++;
       cuts_pb[1.]=i;
   }
-  if(cuts_pb.count(0.)==0) cuts_pb[0.]=f.cuts.size();
-  if(cuts_pb.count(1.)==0) cuts_pb[1.]=f.cuts.size();
-  
-  //-- Compose each piece with the relevant seg.
-  result.push_cut(0.);
-  std::map<double,int>::iterator cut=cuts_pb.begin();
-  std::map<double,int>::iterator next=cut; next++;
+ 
+  //-- Compose each piece of g with the relevant seg of f.
+  result.cuts.push_back(0.);
+  std::map<double,unsigned>::iterator cut=cuts_pb.begin();
+  std::map<double,unsigned>::iterator next=cut; next++;
   while(next!=cuts_pb.end()){
     double t0=(*cut).first;
     int  idx0=(*cut).second;
     double t1=(*next).first;
     int  idx1=(*next).second;
     int  idx; //idx of the relevant f.segs
-    if (std::max(idx0,idx1)==f.cuts.size()){ //g([t0,t1]) is above the top level,
-      idx=f.cuts.size()-2;
-    }else if (std::min(idx0,idx1)==-1){      //g([t0,t1]) is below the min level,
-      idx=0;
+    if (std::max(idx0,idx1)==levels.size()){ //g([t0,t1]) is above the top level,
+      idx=levels.size()-1;
     } else if (idx0 != idx1){                //g([t0,t1]) crosses from level idx0 to idx1,
       idx=std::min(idx0,idx1);
-    } else if(g((t0+t1)/2) < f.cuts[idx0]) { //g([t0,t1]) is a 'U' under level idx0,
+    } else if(g((t0+t1)/2) < levels[idx0]) { //g([t0,t1]) is a 'U' under level idx0,
       idx=idx0-1;
-    } else if(g((t0+t1)/2) > f.cuts[idx0]) { //g([t0,t1]) is a 'bump' over level idx0,
+    } else if(g((t0+t1)/2) > levels[idx0]) { //g([t0,t1]) is a 'bump' over level idx0,
       idx=idx0;
     } else {                                 //g([t0,t1]) is contained in level idx0!...
-      idx = (idx0==f.cuts.size())? idx0-1:idx0;
+      idx = (idx0==levels.size())? idx0-1:idx0;
     }
 
-    if (idx==-1) idx=0;
-    if (idx>=f.cuts.size()-1) idx=f.cuts.size()-2;
+    //move idx back from levels f.cuts 
+    idx+=1;
 
-    if (idx>=0 and idx<f.cuts.size()) {
-      SBasis sub_g=compose(g, BezOrd(t0,t1));
-      sub_g=compose(BezOrd(-f.cuts[idx]/(f.cuts[idx+1]-f.cuts[idx]),
+    SBasis sub_g=compose(g, BezOrd(t0,t1));
+    sub_g=compose(BezOrd(-f.cuts[idx]/(f.cuts[idx+1]-f.cuts[idx]),
 			     (1-f.cuts[idx])/(f.cuts[idx+1]-f.cuts[idx])),sub_g);
-      sub_g=compose(f[idx],sub_g);
-      result.push(sub_g, t1);
-    }
+    sub_g=compose(f[idx],sub_g);
+    result.cuts.push_back(t1);
+    result.segs.push_back(sub_g);
     cut++;
     next++;
   }
