eff29a7f2e5e474e15a3203cafbdecfb0305b5a1
Michael G. Sloan <mgsloan@gmail.com>
2007-03-13 03:45:25 +0000

    Removal of diminsion generality

--- src/multidim.h
+++ src/multidim.h
@@ -6,10 +6,10 @@
 
 namespace Geom{
 
-template <typename T, unsigned D>
-class Multidim{
+template <typename T>
+class D2{
 public:
-    T f[D];
+    T f[2];
     
     T& operator[](unsigned i) {
         return f[i];
@@ -20,204 +20,197 @@ public:
 
     unsigned size() const {
         unsigned s = f[0].size();
-        for(unsigned i = 1; i < D; i++)
+        for(unsigned i = 1; i < 2; i++)
             s = std::max(s, (unsigned) f[i].size());
         return s;
     }
     
     double tail_error(unsigned tail) const {
         double s = f[0].tail_error(tail);
-        for(unsigned i = 1; i < D; i++)
+        for(unsigned i = 1; i < 2; i++)
             s = std::max(s, f[i].tail_error(tail));
         return s;
     }
 
     bool is_finite() const {
-        for(unsigned i = 0; i < D; i++)
+        for(unsigned i = 0; i < 2; i++)
             if(!f[i].is_finite())
                 return false;
         return true;
     }
 };
 
-template <typename T1, typename T2, unsigned D>
-inline Multidim<T1, D>
-operator +(Multidim<T1, D> const & a, Multidim<T2, D> const & b) {
-    Multidim<T1, D> r;
-    for(unsigned i = 0; i < D; i++)
+template <typename T1, typename T2>
+inline D2<T1>
+operator +(D2<T1> const & a, D2<T2> const & b) {
+    D2<T1> r;
+    for(unsigned i = 0; i < 2; i++)
         r.f[i] = a.f[i] + b.f[i];
     return r;
 }
 
-template <typename T1, typename T2, unsigned D>
-inline Multidim<T1, D>
-operator -(Multidim<T1, D> const & a, Multidim<T2, D> const & b) {
-    Multidim<T1, D> r;
-    for(unsigned i = 0; i < D; i++)
+template <typename T1, typename T2>
+inline D2<T1>
+operator -(D2<T1> const & a, D2<T2> const & b) {
+    D2<T1> r;
+    for(unsigned i = 0; i < 2; i++)
         r.f[i] = a.f[i] - b.f[i];
     return r;
 }
 
-template <typename T, unsigned D>
-inline Multidim<T, D>
-operator *(double a, Multidim<T, D> const & b) {
-    Multidim<T, D> r;
-    for(unsigned i = 0; i < D; i++)
+template <typename T>
+inline D2<T>
+operator *(double a, D2<T> const & b) {
+    D2<T> r;
+    for(unsigned i = 0; i < 2; i++)
         r.f[i] = a*b.f[i];
     return r;
 }
 
-template <typename T, unsigned D>
-inline Multidim<T, D>
-operator +=(Multidim<T, D> & a, Multidim<T, D> const & b) {
-    for(unsigned i = 0; i < D; i++)
+template <typename T>
+inline D2<T>
+operator +=(D2<T> & a, D2<T> const & b) {
+    for(unsigned i = 0; i < 2; i++)
         a.f[i]+=b.f[i];
     return a;
 }
 
-template <typename T, unsigned D>
-inline Multidim<T, D>
-operator *=(Multidim<T, D> & a, double b) {
-    for(unsigned i = 0; i < D; i++)
+template <typename T>
+inline D2<T>
+operator *=(D2<T> & a, double b) {
+    for(unsigned i = 0; i < 2; i++)
         a.f[i]*=b;
     return a;
 }
 
-/*template <typename T, unsigned D>
-Multidim<T, D>
-operator -=(Multidim<T, D> & a, Multidim<T, D> const & b) {
-    for(unsigned i = 0; i < D; i++)
+/*template <typename T>
+D2<T>
+operator -=(D2<T> & a, D2<T> const & b) {
+    for(unsigned i = 0; i < 2; i++)
         a.f[i]-=b.f[i];
     return a;
 }*/
 
-template <unsigned D>
-inline Multidim<SBasis, D>
-derivative(Multidim<SBasis, D> const & a) {
-    Multidim<SBasis, D> r;
-    for(unsigned i = 0; i < D; i++)
+inline D2<SBasis>
+derivative(D2<SBasis> const & a) {
+    D2<SBasis> r;
+    for(unsigned i = 0; i < 2; i++)
         r.f[i]=derivative(a.f[i]);
     return r;
 }
 
-template <unsigned D>
-inline Multidim<SBasis, D>
-integral(Multidim<SBasis, D> const & a) {
-    Multidim<SBasis, D> r;
-    for(unsigned i = 0; i < D; i++)
+inline D2<SBasis>
+integral(D2<SBasis> const & a) {
+    D2<SBasis> r;
+    for(unsigned i = 0; i < 2; i++)
         r.f[i]=integral(a.f[i]);
     return r;
 }
 
-template <unsigned D>
+template <typename T>
 inline SBasis
-dot(Multidim<SBasis, D> const & a, Multidim<SBasis, D> const & b) {
+dot(D2<T> const & a, D2<T> const & b) {
     T r;
-    for(unsigned i = 0; i < D; i++)
-        r += multiply(a.f[i], b.f[i]);
+    for(unsigned i = 0; i < 2; i++)
+        r += a.f[i] * b.f[i];
     return r;
 }
 
 //These two L2s are the same except the k param
 
-template <unsigned D>
 inline SBasis
-L2(Multidim<SBasis, D> const & a, int k) {
+L2(D2<SBasis> const & a, int k) {
     double r;
-    for(unsigned i = 0; i < D; i++)
+    for(unsigned i = 0; i < 2; i++)
         r += multiply(a.f[i], a.f[i]);
     return sqrt(r,k);
 }
 
-template <unsigned D>
 inline double
-L2(Multidim<double, D> const & a) {
+L2(D2<double> const & a) {
     double r;
-    for(unsigned i = 0; i < D; i++)
+    for(unsigned i = 0; i < 2; i++)
         r += multiply(a.f[i], a.f[i]);
     return sqrt(r);
 }
 
-template <unsigned D>
-inline Multidim<SBasis, D>
-multiply(BezOrd const & a, Multidim<SBasis, D> const & b) {
-    Multidim<SBasis, D> r;
-    for(unsigned i = 0; i < D; i++)
+inline D2<SBasis>
+multiply(BezOrd const & a, D2<SBasis> const & b) {
+    D2<SBasis> r;
+    for(unsigned i = 0; i < 2; i++)
         r[i] = multiply(a, b.f[i]);
     return r;
 }
 
-template <typename T, unsigned D>
-inline Multidim<T, D>
-multiply(T const & a, Multidim<T, D> const & b) {
-    Multidim<T, D> r;
-    for(unsigned i = 0; i < D; i++)
+template <typename T>
+inline D2<T>
+multiply(T const & a, D2<T> const & b) {
+    D2<T> r;
+    for(unsigned i = 0; i < 2; i++)
         r[i] = multiply(a, b.f[i]);
     return r;
 }
 
-template <unsigned D>
-inline Multidim<SBasis, D>
-operator*(BezOrd const & a, Multidim<SBasis, D> const & b) {
+inline D2<SBasis>
+operator*(BezOrd const & a, D2<SBasis> const & b) {
     return multiply(a, b);
 }
 
-template <typename T, unsigned D>
-inline Multidim<T, D>
-operator*(T const & a, Multidim<T, D> const & b) {
+template <typename T>
+inline D2<T>
+operator*(T const & a, D2<T> const & b) {
     return multiply(a, b);
 }
 
 /* Doesn't match composition
-template <typename T, unsigned D>
-inline Multidim<T, D>
-compose(T const & a, Multidim<T, D> const & b) {
-    Multidim<T, D> r;
-    for(unsigned i = 0; i < D; i++)
+template <typename T>
+inline D2<T>
+compose(T const & a, D2<T> const & b) {
+    D2<T> r;
+    for(unsigned i = 0; i < 2; i++)
         r[i] = compose(a,b.f[i]);
     return r;
 }
 */
 
 //Only applies to func types, hard to encode this with templates
-template <typename T, unsigned D>
-inline Multidim<T, D>
-compose(Multidim<T, D> const & a, T const & b) {
-    Multidim<T, D> r;
-    for(unsigned i = 0; i < D; i++)
+template <typename T>
+inline D2<T>
+compose(D2<T> const & a, T const & b) {
+    D2<T> r;
+    for(unsigned i = 0; i < 2; i++)
         r[i] = compose(a.f[i],b);
     return r;
 }
 
-template <typename T, unsigned D>
-inline Multidim<T, D>
-composeEach(Multidim<T, D> const & a, Multidim<T, D> const & b) {
-    Multidim<T, D> r;
-    for(unsigned i = 0; i < D; i++)
+template <typename T>
+inline D2<T>
+composeEach(D2<T> const & a, D2<T> const & b) {
+    D2<T> r;
+    for(unsigned i = 0; i < 2; i++)
         r[i] = compose(a.f[i],b.f[i]);
     return r;
 }
 
-template <unsigned D>
-inline Multidim<SBasis, D>
-truncate(Multidim<SBasis, D> const & a, unsigned terms) {
-    Multidim<SBasis, D> r;
-    for(unsigned i = 0; i < D; i++)
+inline D2<SBasis>
+truncate(D2<SBasis> const & a, unsigned terms) {
+    D2<SBasis> r;
+    for(unsigned i = 0; i < 2; i++)
         r[i] = truncate(a.f[i], terms);
     return r;
 }
 
-inline Multidim<T, 2>
-rot90(Multidim<T, 2> const & a) {
-    Multidim<T, 2> r;
+inline D2<T>
+rot90(D2<T> const & a) {
+    D2<T> r;
     r.f[0] = -a.f[1];
     r.f[1] = a.f[0];
     return r;
 }
 
-inline Multidim<T, 2>
-cross(Multidim<T, 2> const & a, Multidim<T, 2> const & b) {
-    Multidim<T, 2> r;
+inline D2<T>
+cross(D2<T> const & a, D2<T> const & b) {
+    D2<T> r;
     r[0] = (-1) * a.f[0] * b.f[1];
     r[1] = a.f[1] * b.f[0];
     return r;
@@ -226,7 +219,7 @@ cross(Multidim<T, 2> const & a, Multidim<T, 2> const & b) {
 //Only applies to (->Double) function types
 template <typename T>
 inline Point
-operator()(Multidim<T, 2> const & a, double t) {
+operator()(D2<T> const & a, double t) {
     return Point(a[0](t), a[1](t);
 }
 
