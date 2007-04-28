a2684603edaf92a572ef9da9accea70996157aed
JF Barraud <jf.barraud@gmail.com>
2007-04-28 21:24:55 +0000

    SBasis.PointAt(double t) tests if t==0 or t==1 to avoid computations in this case.

--- src/s-basis.h
+++ src/s-basis.h
@@ -71,6 +71,9 @@ public:
     }
 
     double pointAt(double t) const {
+        if (empty()) return 0;
+        if(t==0.) return (*this)[0][0];
+        if(t==1.) return (*this)[0][1];
         double s = t*(1-t);
         double p0 = 0, p1 = 0;
         double sk = 1;
