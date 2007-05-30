afe73f9ece55c89d0e11f3c09f6e8f715307241a
Johan B. C. Engelen <jbc.engelen@swissonline.ch>
2007-05-30 19:16:18 +0000

    Added win32 instructions on how to compile with Ragel. (SVG parser)

--- mingwenv.bat
+++ mingwenv.bat
@@ -3,5 +3,6 @@ set MINGW_BIN=c:\mingw\bin
 set CMAKE_BIN=c:\cmake\bin
 set GTK_BIN=c:\gtk210\bin
 set PKG_CONFIG_PATH=c:\gtk210\lib\pkgconfig
-set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%PATH%
+set RAGEL_BIN=c:\ragel
+set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%RAGEL_BIN%;%PATH%
 set BOOST_DIR=c:\boost
