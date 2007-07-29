fda5ce5c11e9b3dda41a5c818e5aa15f09f65e42
Michael G. Sloan <mgsloan@gmail.com>
2007-07-29 04:30:19 +0000

    Looks like I forgot to add the test.  Really I didn't forget - on my last repo I did add it, but svn refused to commit on some technicallity.

---
+++ src/toys/winding-test.cpp
@@ -0,0 +1,48 @@
+#include "path.h"
+#include "svg-path-parser.h"
+
+#include <iostream>
+
+#include "path-cairo.h"
+#include "toy-framework.cpp"
+
+using namespace Geom;
+
+int winding(vector<Path> ps, Point p) {
+    int wind = 0;
+    for(unsigned i = 0; i < ps.size(); i++) {
+        wind += abs(ps[i].winding(p));
+    }
+    return wind;
+}
+
+class WindingTest: public Toy {
+    vector<Path> path;
+    virtual void draw(cairo_t *cr, std::ostringstream *notify, int width, int height, bool save) {
+        cairo_path(cr, path);
+        std::cout << "width:" << winding(path, handles[0]) << "\n";
+        Toy::draw(cr, notify, width, height, save);
+    }
+
+    public:
+    WindingTest () {
+        path = read_svgd("parametrics.svgd");
+        handles.push_back(Point(300,300));
+    }
+};
+
+int main(int argc, char **argv) {
+    init(argc, argv, new WindingTest());
+    return 0;
+}
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4:encoding=utf-8:textwidth=99 :
