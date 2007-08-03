d6b37b17876416522fda6e0777cd945099dab149
Michael G. Sloan <mgsloan@gmail.com>
2007-08-03 01:21:35 +0000

    Boolops now demonstrates both intersection and union

--- src/toys/boolops.cpp
+++ src/toys/boolops.cpp
@@ -13,9 +13,16 @@
 
 using namespace Geom;
 
-void cairo_shape(cairo_t *cr, Shapes s) {
+void cairo_paths(cairo_t *cr, Paths p) {
+    for(Paths::iterator j = p.begin(); j != p.end(); j++) {
+        cairo_path(cr, *j);
+    }
+}
+
+void cairo_shapes(cairo_t *cr, Shapes s) {
     for(unsigned i = 0; i < s.size(); i++) {
         cairo_path(cr, s[i].getOuter());
+        cairo_paths(cr, s[i].getHoles());
     }
 }
 
@@ -23,7 +30,6 @@ void mark_crossings(cairo_t *cr, Path const &a, Path const &b) {
     Crossings c = crossings(a, b);
     for(Crossings::iterator i = c.begin(); i != c.end(); i++) {
         draw_cross(cr, a.pointAt(i->ta));
-        draw_cross(cr, Point(i->ta * 10, i->ta * 10));
         draw_text(cr, a.pointAt(i->ta), i->dir ? "T" : "F");
     }
 }
@@ -52,26 +58,35 @@ class BoolOps: public Toy {
     virtual void draw(cairo_t *cr, std::ostringstream *notify, int width, int height, bool save) {
         //Paths none;
         //Shape a(path_a.front(), none), b(path_b.front(), none);
-        Path a(path_a.front()), b(path_b.front() * Matrix(1, 0, 0, 1, handles[0][X], handles[0][Y]));
+        Path a(path_a.front()), b(path_b.front() * Matrix(3, 0, 0, 3, handles[0][X]-550, handles[0][Y]-250));
         cairo_path(cr, a);
         cairo_path(cr, b);
         cairo_stroke(cr);
         
-        Path port = a.portion(handles[1][X] / 100., handles[2][X] / 100.);
+        /*Path port = a.portion(handles[1][X] / 100., handles[2][X] / 100.);
         cairo_set_source_rgba(cr, 0., 1., 0., 1.);
         cairo_path(cr, port);
-        cairo_stroke(cr);
+        cairo_stroke(cr); */
         
         mark_crossings(cr, a, b);
         draw_bounds(cr, a);
         draw_bounds(cr, b);
         //std::streambuf* cout_buffer = std::cout.rdbuf();
         //std::cout.rdbuf(notify->rdbuf());
-        Shapes res = path_union(a, b);
-        cairo_set_source_rgba(cr, 1., 0., 0., 1);
-        cairo_shape(cr, res);
+        cairo_set_line_width(cr, 5);
+        
+        Shapes uni = path_union(a, b);
+        cairo_set_source_rgba(cr, 1., 0., 0., .5);
+        cairo_shapes(cr, uni);
+        cairo_stroke(cr);
+        
+        Paths inte = path_intersect(a, b);
+        cairo_set_source_rgba(cr, 0., 1., 0., .5);
+        cairo_paths(cr, inte);
         cairo_stroke(cr);
         //std::cout.rdbuf(cout_buffer);
+
+        cairo_set_line_width(cr, 1);
 
         Toy::draw(cr, notify, width, height, save);
     }
@@ -81,8 +96,8 @@ class BoolOps: public Toy {
         path_a = read_svgd("winding.svgd");
         path_b = read_svgd("monk.svgd");
         handles.push_back(Point(300,300));
-        handles.push_back(Point(200,300));
-        handles.push_back(Point(250,300));
+        //handles.push_back(Point(200,300));
+        //handles.push_back(Point(250,300));
     }
 };
 
