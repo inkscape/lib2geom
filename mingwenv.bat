d0e7e5c3368d6ae4b0fabd678c59245d815b768b
Johan B. C. Engelen <jbc.engelen@swissonline.ch>
2008-02-13 22:17:21 +0000

    update mingwenv.bat to latest inkscape dev packages. :-)

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,8 +1,8 @@
 @echo Setting environment variables for MinGw build of lib2geom
 set MINGW_BIN=c:\mingw\bin
 set CMAKE_BIN=c:\cmake\bin
-set GTK_BIN=c:\gtk210\bin
-set PKG_CONFIG_PATH=c:\gtk210\lib\pkgconfig
+set GTK_BIN=c:\devlibs\bin
+set PKG_CONFIG_PATH=c:\devlibs\lib\pkgconfig
 set RAGEL_BIN=c:\ragel
 set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%RAGEL_BIN%;%PATH%
 set BOOST_DIR=c:\boost
