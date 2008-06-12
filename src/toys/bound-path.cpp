8b2d5347421f539f0bb2232befa29a34f1a755ad
Marco Cechetti <mrcekets@gmail.com>
2008-06-12 21:07:37 +0000

    bound-path complete implementation with support for bounding pathvectors

--- src/toys/bound-path.cpp
+++ src/toys/bound-path.cpp
@@ -62,39 +62,50 @@ class BoundsPath : public Toy
     void draw( cairo_t *cr, std::ostringstream *notify, 
                int width, int height, bool save )
     {
-        cairo_set_source_rgba(cr, 0.3, 0.3, 0.3, 1.0);
         cairo_set_line_width (cr, 0.3);
         m_selection_kind = (unsigned int) (sliders[0].value());
-        for (unsigned int ii = 0; ii < m_paths_per_vector; ++ii)
+        
+        for (unsigned int i = 0; i < m_pathvector_coll.size(); ++i)
         {
-            m_pathvector[ii].clear();
-            for (unsigned int i = 0; i < m_curves_per_path; ++i)
+            cairo_set_source_rgba(cr, 0.0, 0.4*(i+1), 0.8/(i+1), 1.0);
+            for (unsigned int j = 0; j < m_pathvector_coll[i].size(); ++j)
             {
-                PointSetHandle psh;
-                psh.pts.resize(m_handles_per_curve);
-                for (unsigned int j = 0; j < m_handles_per_curve; ++j)
+                m_pathvector_coll[i][j].clear();
+                for (unsigned int k = 0; k < m_curves_per_path; ++k)
                 {
-                    unsigned int k = i * (m_handles_per_curve-1) + j;
-                    psh.pts[j] = m_pathvector_handles[ii].pts[k];
+                    PointSetHandle psh;
+                    psh.pts.resize(m_handles_per_curve);
+                    for (unsigned int h = 0; h < m_handles_per_curve; ++h)
+                    {
+                        unsigned int kk = k * (m_handles_per_curve-1) + h;
+                        psh.pts[h] = m_pathvector_coll_handles[i][j].pts[kk];
+                    }
+                    m_pathvector_coll[i][j].append(psh.asBezier());
                 }
-                m_pathvector[ii].append(psh.asBezier());
+                cairo_path(cr, m_pathvector_coll[i][j]);
             }
-            cairo_path(cr, m_pathvector[ii]);
+            cairo_stroke(cr);
         }
-        cairo_stroke(cr);
+        
         
         Rect bound;
         if ( (m_selection_kind == CURVE) && (m_selected_curve != -1) )
         {
-            const Curve & curve = m_pathvector[m_selected_path][m_selected_curve];
+            const Curve & curve = m_pathvector_coll[m_selected_pathvector][m_selected_path][m_selected_curve];
             bound = toggles[0].on ? curve.boundsExact()
                                   : curve.boundsFast();
         }
         else if ( (m_selection_kind == PATH) && (m_selected_path != -1) )
         {
-            const Path & path = m_pathvector[m_selected_path];
+            const Path & path = m_pathvector_coll[m_selected_pathvector][m_selected_path];
             bound = toggles[0].on ? path.boundsExact()
                                   : path.boundsFast();
+        }
+        else if ( (m_selection_kind == PATHVECTOR) && (m_selected_pathvector != -1) )
+        {
+            const PathVector & pathvector = m_pathvector_coll[m_selected_pathvector];
+            bound = toggles[0].on ? bounds_exact(pathvector)
+                                  : bounds_fast(pathvector);            
         }
 
         cairo_set_source_rgba(cr, 0.5, 0.0, 0.0, 1.0);
@@ -117,29 +128,27 @@ class BoundsPath : public Toy
         m_selected_curve = -1;
         if (m_selection_kind == CURVE)
         {
-            for (unsigned int i = 0; i < m_pathvector.size(); ++i)
+            for (unsigned int i = 0; i < m_pathvector_coll.size(); ++i)
             {
-                for ( unsigned int j = 0; j <  m_pathvector[i].size(); ++j)
+                for (unsigned int j = 0; j < m_pathvector_coll[i].size(); ++j)
                 {
-                    if (toggles[0].on == EXACT)
+                    for ( unsigned int k = 0; k <  m_pathvector_coll[i][j].size(); ++k)
                     {
-                        bound = m_pathvector[i][j].boundsExact();
-                    }
-                    else
-                    {
-                        bound = m_pathvector[i][j].boundsFast();
-                    }
-                    d = distanceSq(pos, bound);
-                    if ( are_near(d, 0) )
-                    {
-                        t = m_pathvector[i][j].nearestPoint(pos);
-                        d = distanceSq(pos, m_pathvector[i][j].pointAt(t));
-                        if (d < dist)
+                        const Curve & curve = m_pathvector_coll[i][j][k];
+                        bound = toggles[0].on ? curve.boundsExact()
+                                              : curve.boundsFast();
+                        d = distanceSq(pos, bound);
+                        if ( are_near(d, 0) )
                         {
-                            dist = d;
-                            m_selected_pathvector = 0;
-                            m_selected_path = i;
-                            m_selected_curve = j;
+                            t = curve.nearestPoint(pos);
+                            d = distanceSq(pos, curve.pointAt(t));
+                            if (d < dist)
+                            {
+                                dist = d;
+                                m_selected_pathvector = i;
+                                m_selected_path = j;
+                                m_selected_curve = k;
+                            }
                         }
                     }
                 }
@@ -149,37 +158,60 @@ class BoundsPath : public Toy
         }
         else if (m_selection_kind == PATH)
         {
-            for (unsigned int i = 0; i < m_pathvector.size(); ++i)
+            for (unsigned int i = 0; i < m_pathvector_coll.size(); ++i)
             {
-                if (toggles[0].on == EXACT)
+                for (unsigned int j = 0; j < m_pathvector_coll[i].size(); ++j)
                 {
-                    bound = m_pathvector[i].boundsExact();
-                }
-                else
-                {
-                    bound = m_pathvector[i].boundsFast();
-                }
-                d = distanceSq(pos, bound);
-                if ( are_near(d, 0) )
-                {
-                    t = m_pathvector[i].nearestPoint(pos);
-                    d = distanceSq(pos, m_pathvector[i].pointAt(t));
-                    if (d < dist)
+                    const Path & path = m_pathvector_coll[i][j];
+                    bound = toggles[0].on ? path.boundsExact()
+                                          : path.boundsFast();
+                    d = distanceSq(pos, bound);
+                    if ( are_near(d, 0) )
                     {
-                        dist = d;
-                        m_selected_pathvector = 0;
-                        m_selected_path = i; 
+                        t = path.nearestPoint(pos);
+                        d = distanceSq(pos, path.pointAt(t));
+                        if (d < dist)
+                        {
+                            dist = d;
+                            m_selected_pathvector = i;
+                            m_selected_path = j; 
+                        }
                     }
                 }
             }
         }
+        else if (m_selection_kind == PATHVECTOR)
+        {
+            for (unsigned int i = 0; i < m_pathvector_coll.size(); ++i)
+            {
+                const PathVector & pathvector = m_pathvector_coll[i];
+                bound = toggles[0].on ? bounds_exact(pathvector)
+                                      : bounds_fast(pathvector);
+                d = distanceSq(pos, bound);
+                if ( are_near(d, 0) )
+                {
+                    for (unsigned int j = 0; j < m_pathvector_coll[i].size(); ++j)
+                    {
+                        const Path & path = m_pathvector_coll[i][j];
+                        t = path.nearestPoint(pos);
+                        d = distanceSq(pos, path.pointAt(t));
+                        if (d < dist)
+                        {
+                            dist = d;
+                            m_selected_pathvector = i;
+                        }
+                    }                    
+                }
+            }
+        }           
+
         Toy::mouse_pressed(e);
     }
  
   public:
     BoundsPath()
     {
-        m_total_pathvectors = 1;
+        m_total_pathvectors = 2;
         m_paths_per_vector = 2;
         m_curves_per_path = 3;
         m_handles_per_curve = 4;
@@ -190,15 +222,21 @@ class BoundsPath : public Toy
         m_selected_curve = -1;
         m_handles_per_path = m_curves_per_path * (m_handles_per_curve-1) + 1;
         
-        m_pathvector_handles.resize(m_paths_per_vector);
-        m_pathvector.resize(m_paths_per_vector);
-        for (unsigned int j = 0; j < m_paths_per_vector; ++j)
+        m_pathvector_coll_handles.resize(m_total_pathvectors);
+        m_pathvector_coll.resize(m_total_pathvectors);
+        for (unsigned int k = 0; k < m_total_pathvectors; ++k)
         {
-            m_pathvector_handles[j].pts.resize(m_handles_per_path);
-            handles.push_back(&(m_pathvector_handles[j]));
-            for (unsigned int i = 0; i < m_handles_per_path; ++i)
+            m_pathvector_coll_handles[k].resize(m_paths_per_vector);
+            m_pathvector_coll[k].resize(m_paths_per_vector);
+            for (unsigned int j = 0; j < m_paths_per_vector; ++j)
             {
-                m_pathvector_handles[j].pts[i] = Point(780*uniform() + 10, 480*uniform() + 80);
+                m_pathvector_coll_handles[k][j].pts.resize(m_handles_per_path);
+                handles.push_back(&(m_pathvector_coll_handles[k][j]));
+                for (unsigned int i = 0; i < m_handles_per_path; ++i)
+                {
+                    m_pathvector_coll_handles[k][j].pts[i] 
+                        = Point(500*uniform() + 300*k, 300*uniform() + 80 + 200*j);
+                }
             }
         }
         
@@ -220,8 +258,10 @@ class BoundsPath : public Toy
     unsigned int m_curves_per_path;
     unsigned int m_handles_per_curve;
     unsigned int m_handles_per_path;
-    PathVector m_pathvector;
-    std::vector<PointSetHandle> m_pathvector_handles;
+    std::vector<PathVector>  m_pathvector_coll;
+    std::vector< std::vector<PointSetHandle> > m_pathvector_coll_handles;
+//    PathVector m_pathvector;
+//    std::vector<PointSetHandle> m_pathvector_handles;
     int m_selected_curve;
     int m_selected_path;
     int m_selected_pathvector;
