0c2b1bba3b22fcdc8771534e49bfc87327859500
Nathan Hurst <njh@njhurst.com>
2008-10-26 23:04:36 +0000

    sequential fitter

--- src/2geom/toys/sb-to-bez.cpp
+++ src/2geom/toys/sb-to-bez.cpp
@@ -107,9 +107,9 @@ sb_seg_to_bez(Piecewise<D2<SBasis> > const &M,double t0,double t1){
 }
 #include <2geom/sbasis-to-bezier.h>
 
-void recursive_curvature_fitter(cairo_t* cr, Piecewise<D2<SBasis> > const &f, double t0, double t1, double precision) {
-      if (t0>=t1) return;//TODO: fix me...
-      if (t0+0.001>=t1) return;//TODO: fix me...
+int recursive_curvature_fitter(cairo_t* cr, Piecewise<D2<SBasis> > const &f, double t0, double t1, double precision) {
+      if (t0>=t1) return 0;//TODO: fix me...
+      if (t0+0.001>=t1) return 0;//TODO: fix me...
       
       Piecewise<D2<SBasis> > g = Piecewise<D2<SBasis> >(f);
       
@@ -120,14 +120,17 @@ void recursive_curvature_fitter(cairo_t* cr, Piecewise<D2<SBasis> > const &f, do
       if(k_bez[0].size() > 1 and k_bez[1].size() > 1) {
           Piecewise<SBasis> s = arcLengthSb(k_bez)+t0;
           Rect bnds = bounds_fast(compose(f,s) - Piecewise<D2<SBasis> >(k_bez));
-          double h_dist = bnds.dimensions().length();//0 is in the rect!, TODO:gain factor ~2 for free.
+          //double h_dist = bnds.dimensions().length();
+//0 is in the rect!, TODO:gain factor ~2 for free.
+// njh: not really, the benefit is actually rather small.
+          double h_dist = max(bnds.min().length(), bnds.max().length());
           
           //printf("(%g %g), %g\n", t0, t1, h_dist);
           //double h_dist = hausdorfl2( cr, k_bez, f, 1e-6, &h_a_t, &h_b_t);
           //double h_dist = hausdorfl( k_bez, f, 1e-6, &h_a_t, &h_b_t);
           if(h_dist > precision) {
               //TODO: find a better place where to cut (at the worst fit?).
-              recursive_curvature_fitter(cr, f, t0, (t0+t1)/2, precision);
+              return recursive_curvature_fitter(cr, f, t0, (t0+t1)/2, precision) +
               recursive_curvature_fitter(cr, f, (t0+t1)/2, t1, precision);
           } 
           else
@@ -152,22 +155,116 @@ void recursive_curvature_fitter(cairo_t* cr, Piecewise<D2<SBasis> > const &f, do
               cairo_md_sb(cr, k_bez);
               cairo_stroke(cr);
               cairo_restore(cr);
+              return 1;
           }
       } else {
-          recursive_curvature_fitter(cr, f, t0, (t0+t1)/2, precision);
+          return recursive_curvature_fitter(cr, f, t0, (t0+t1)/2, precision) +
           recursive_curvature_fitter(cr, f, (t0+t1)/2, t1, precision);
       } 
 }
+
+double single_curvature_fitter(Piecewise<D2<SBasis> > const &f, double t0, double t1, double precision) {
+      if (t0>=t1) return 0;//TODO: fix me...
+      if (t0+0.001>=t1) return 0;//TODO: fix me...
+      
+      D2<SBasis> k_bez = sb_seg_to_bez(f,t0,t1);      
+
+      if(k_bez[0].size() > 1 and k_bez[1].size() > 1) {
+          Piecewise<SBasis> s = arcLengthSb(k_bez)+t0;
+          Rect bnds = bounds_fast(compose(f,s) - Piecewise<D2<SBasis> >(k_bez));
+          double h_dist = max(bnds.min().length(), bnds.max().length());
+          
+          return h_dist;
+      }
+      return 1e100;
+}
+
+struct quadratic_params
+{
+    Piecewise<D2<SBasis> > const *f;
+    double t0, precision;
+};
+     
+
+double quadratic (double x, void *params) {
+    struct quadratic_params *p 
+        = (struct quadratic_params *) params;
+     
+    return single_curvature_fitter(*p->f, p->t0, x, p->precision) - p->precision;
+}
+     
+#include <stdio.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_roots.h>
+     
+     
+void sequential_curvature_fitter(cairo_t* cr, Piecewise<D2<SBasis> > const &f, double t0, double t1, double precision) {
+    if(t0 >= t1) return;
+    
+    double r = t1;
+    if(single_curvature_fitter(f, t0, t1, precision) > precision) {
+        int status;
+        int iter = 0, max_iter = 100;
+        const gsl_root_fsolver_type *T;
+        gsl_root_fsolver *s;
+        gsl_function F;
+        struct quadratic_params params = {&f, t0, precision};
+     
+        F.function = &quadratic;
+        F.params = &params;
+     
+        T = gsl_root_fsolver_brent;
+        s = gsl_root_fsolver_alloc (T);
+        gsl_root_fsolver_set (s, &F, t0, t1);
+     
+        do
+        {
+            iter++;
+            status = gsl_root_fsolver_iterate (s);
+            r = gsl_root_fsolver_root (s);
+            double x_lo = gsl_root_fsolver_x_lower (s);
+            double x_hi = gsl_root_fsolver_x_upper (s);
+            status = gsl_root_test_interval (x_lo, x_hi,
+                                             0, 0.001);
+     
+     
+        }
+        while (status == GSL_CONTINUE && iter < max_iter);
+    
+        double x_lo = gsl_root_fsolver_x_lower (s);
+        double x_hi = gsl_root_fsolver_x_upper (s);
+        printf ("%5d [%.7f, %.7f] %.7f %.7f\n",
+                iter, x_lo, x_hi,
+                r, 
+                x_hi - x_lo);
+        gsl_root_fsolver_free (s);
+    }    
+    D2<SBasis> k_bez = sb_seg_to_bez(f,t0,r);      
+    
+    cairo_save(cr);
+    cairo_set_line_width (cr, 0.93);
+    cairo_set_source_rgba (cr, 0.7, 0.0, 0.0, 1);
+    draw_handle(cr, k_bez.at0());
+    cairo_md_sb(cr, k_bez);
+    cairo_stroke(cr);
+    cairo_restore(cr);
+    
+    if(r < t1)
+        sequential_curvature_fitter(cr, f, r, t1, precision);
+}
+
 
 class SbToBezierTester: public Toy {
     //std::vector<Slider> sliders;
     PointSetHandle path_psh[2];
     PointHandle adjuster, adjuster2, adjuster3;
+    std::vector<Toggle> toggles;
 
   void draw(cairo_t *cr, std::ostringstream *notify, int width, int height, bool save) {
       cairo_save(cr);
       Piecewise<D2<SBasis> >f_as_pw = Piecewise<D2<SBasis> >(path_psh[0].asBezier());
-      f_as_pw.push_seg(path_psh[1].asBezier());
+      //f_as_pw.push_seg(path_psh[1].asBezier());
       //f=handles_to_sbasis(handles.begin(), SIZE-1);
       adjuster.pos[1]=450;
       adjuster.pos[0]=std::max(adjuster.pos[0],150.);
@@ -242,16 +339,32 @@ class SbToBezierTester: public Toy {
       double curve_precision = pow(10, scale0*5-2);
       val_s << curve_precision;
       draw_text(cr, adjuster2.pos, val_s.str().c_str());
-
-      recursive_curvature_fitter(cr, f_as_pw, 0, f_as_pw.cuts.back(),curve_precision);
+      
+      if(toggles[0].on)
+          sequential_curvature_fitter(cr, f_as_pw, 0, f_as_pw.cuts.back(), curve_precision);
+      else {
+          int segs = recursive_curvature_fitter(cr, f_as_pw, 0, f_as_pw.cuts.back(),curve_precision);
+          *notify << "      total segments: "<< segs <<"\n";
+      }
       cairo_restore(cr);
+      Point p(25, height - 50), d(50,25);
+      toggles[0].bounds = Rect(p,     p + d);
+      draw_toggles(cr, toggles);
       Toy::draw(cr, notify, width, height, save);
   }
   
 public:
+    void key_hit(GdkEventKey *e) {
+        if(e->keyval == 's') toggles[0].toggle();
+        redraw();
+    }
+    void mouse_pressed(GdkEventButton* e) {
+        toggle_events(toggles, e);
+        Toy::mouse_pressed(e);
+    }
     SbToBezierTester() {
       //if(handles.empty()) {
-        for(int j = 0; j < 2; j++) {
+        for(int j = 0; j < 1; j++) {
             for(unsigned i = 0; i < 6; i++)
                 path_psh[j].push_back(150+300*uniform(),150+300*uniform());
             handles.push_back(&path_psh[j]);
@@ -262,6 +375,7 @@ public:
       handles.push_back(&adjuster2);
       adjuster3.pos = Geom::Point(450,300);
       handles.push_back(&adjuster3);
+      toggles.push_back(Toggle("Seq", true));
       //}
     //sliders.push_back(Slider(0.0, 1.0, 0.0, 0.0, "t"));
     //handles.push_back(&(sliders[0]));
