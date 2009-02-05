f5adea5e2dd963054a2303e556a1dcb9200d9cfe
Nathan Hurst <njh@njhurst.com>
2009-02-05 00:08:06 +0000

    portion out inflection points

--- src/2geom/toys/evolute.cpp
+++ src/2geom/toys/evolute.cpp
@@ -26,19 +26,22 @@ virtual void draw(cairo_t *cr, std::ostringstream *notify, int width, int height
     for(int i = 0; i < rts.size(); i++) {
         draw_handle(cr, A(rts[i]));
     }
+    cairo_d2_sb(cr, A);
+    cairo_stroke(cr);
+    Interval r(0, 1);
+    if(rts.size() > 0)
+        r[1] = rts[0];
     //if(rts[0] == 0)
     //rts.erase(rts.begin(), rts.begin()+1);
-    A = portion(A, rts[0], rts[1]);
-    dA = portion(dA, rts[0], rts[1]);
-    ddA = portion(ddA, rts[0], rts[1]);
-    crs = portion(crs, rts[0], rts[1]);
+    A = portion(A, r[0], r[1]);
+    dA = portion(dA, r[0], r[1]);
+    ddA = portion(ddA, r[0], r[1]);
+    crs = portion(crs, r[0], r[1]);
     cairo_stroke(cr);
     Piecewise<SBasis> s = divide(dA[0]*dot(dA,dA), crs, 100, 1);
     D2<Piecewise<SBasis> > ev4(Piecewise<SBasis>(A[0]) + divide(-dA[1]*dot(dA,dA), crs, 100, 1),
                                Piecewise<SBasis>(A[1]) + divide(dA[0]*dot(dA,dA), crs, 100, 1));
     cairo_d2_pw_sb(cr, ev4);
-    cairo_stroke(cr);
-    cairo_d2_sb(cr, A);
     cairo_stroke(cr);
     cairo_set_source_rgba (cr, 1., 0., 1, 1);
     
