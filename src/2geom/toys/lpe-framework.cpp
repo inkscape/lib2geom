68bf0245cd08c43c9d7e167dadf9bc9c610448e7
Johan B. C. Engelen <jbc.engelen@swissonline.ch>
2010-05-01 13:01:16 +0200

    add documentation to the LPE framework

--- src/2geom/toys/lpe-framework.cpp
+++ src/2geom/toys/lpe-framework.cpp
@@ -1,3 +1,44 @@
+/**
+ * \file lpe-framework.cpp
+ * \brief A framework for writing an Inkscape Live Path Effect (LPE) toy. See lpe-test.cpp.
+ *
+ * Instead of using the standard toy framework, one can use this LPE 
+ * framework when creating an LPE for Inkscape. When new 2geom functions 
+ * are required for the LPE, adding this functionality directly in Inkscape 
+ * greatly increases compile times. Using this framework, one only has to
+ * rebuild 2geom, which speeds up development considerably. (Think about
+ * how much of Inkscape will have to be rebuild if you change/add something
+ * in point.h ...)
+ * An example of how to use this framework is lpe.test.cpp.
+ */
+/*
+ * Copyright 2009  Johan Engelen <goejendaagh@zonnet.nl>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it either under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * (the "LGPL") or, at your option, under the terms of the Mozilla
+ * Public License Version 1.1 (the "MPL"). If you do not alter this
+ * notice, a recipient may use your version of this file under either
+ * the MPL or the LGPL.
+ *
+ * You should have received a copy of the LGPL along with this library
+ * in the file COPYING-LGPL-2.1; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the MPL along with this library
+ * in the file COPYING-MPL-1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY
+ * OF ANY KIND, either express or implied. See the LGPL or the MPL for
+ * the specific language governing rights and limitations.
+ *
+ */
+ 
 #include <string.h>
 #include <stdint.h>
 #include <2geom/toys/lpe-framework.h>
@@ -8,6 +49,9 @@
 
 #define  LPE_CONVERSION_TOLERANCE 0.01
 
+/**
+ * When the input path handles are moved, this method is called to re-execute the LPE and draw the result.
+ */
 void LPEToy::draw(cairo_t *cr, std::ostringstream *notify, int width, int height, bool save, std::ostringstream *timer_stream)
 {
     using namespace Geom;
@@ -30,6 +74,9 @@ void LPEToy::draw(cairo_t *cr, std::ostringstream *notify, int width, int height
     Toy::draw(cr, notify, width, height, save,timer_stream);
 }
 
+/**
+ * Initializes the LPE toy, and sets a simple default input path.
+ */
 LPEToy::LPEToy(){
     if(handles.empty()) {
         handles.push_back(&curve_handle);
--- src/2geom/toys/lpe-test.cpp
+++ src/2geom/toys/lpe-test.cpp
@@ -1,11 +1,41 @@
+/**
+ * \file lpe-test.cpp
+ * \brief Example file showing how to write an Inkscape Live Path Effect toy.
+ */
+/*
+ * Copyright 2009  Johan Engelen <goejendaagh@zonnet.nl>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it either under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * (the "LGPL") or, at your option, under the terms of the Mozilla
+ * Public License Version 1.1 (the "MPL"). If you do not alter this
+ * notice, a recipient may use your version of this file under either
+ * the MPL or the LGPL.
+ *
+ * You should have received a copy of the LGPL along with this library
+ * in the file COPYING-LGPL-2.1; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the MPL along with this library
+ * in the file COPYING-MPL-1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY
+ * OF ANY KIND, either express or implied. See the LGPL or the MPL for
+ * the specific language governing rights and limitations.
+ *
+ */
+
 #include <2geom/toys/lpe-framework.h>
 
+// This is usually very bad practice: don't do it in your LPE
 using std::vector;
 using namespace Geom;
 using namespace std;
-
-// Author: Johan Engelen, 2009
-//-----------------------------------------------
 
 class LPETest: public LPEToy {
 public:
