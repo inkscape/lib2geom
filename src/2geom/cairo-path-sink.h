0c2cbe21f8ec2aec21b480964058b0e2c2bd741a
Krzysztof Kosiński <tweenk.pl@gmail.com>
2014-06-24 12:11:21 +0200

    Add a CairoPathSink class that outputs a path to a Cairo drawing context. Use the new class in the load-svgd toy. Add an SVGD file that contains elliptical arc segments.

---
+++ src/2geom/cairo-path-sink.h
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * @brief  Path sink for Cairo contexts
+ *//*
+ * Copyright 2014 Krzysztof Kosiński
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it either under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * (the "LGPL") or, at your option, under the terms of the Mozilla
+ * Public License Version 1.1 (the "MPL"). If you do not alter this
+ * notice, a recipient may use your version of this file under either
+ * the MPL or the LGPL.
+ *
+ * You should have received a copy of the LGPL along with this library
+ * in the file COPYING-LGPL-2.1; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the MPL along with this library
+ * in the file COPYING-MPL-1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY
+ * OF ANY KIND, either express or implied. See the LGPL or the MPL for
+ * the specific language governing rights and limitations.
+ *
+ */
+
+#ifndef SEEN_LIB2GEOM_CAIRO_PATH_SINK_H
+#define SEEN_LIB2GEOM_CAIRO_PATH_SINK_H
+
+#include <2geom/path-sink.h>
+#include <cairo.h>
+
+namespace Geom {
+
+
+/** @brief 
+ */
+class CairoPathSink
+    : public PathSink
+{
+public:
+    CairoPathSink(cairo_t *cr);
+
+    virtual void moveTo(Point const &p);
+    virtual void hlineTo(Coord v);
+    virtual void vlineTo(Coord v);
+    virtual void lineTo(Point const &p);
+    virtual void curveTo(Point const &c0, Point const &c1, Point const &p);
+    virtual void quadTo(Point const &c, Point const &p);
+    virtual void arcTo(double rx, double ry, double angle,
+                       bool large_arc, bool sweep, Point const &p);
+    virtual void closePath();
+    virtual void flush();
+
+private:
+    cairo_t *_cr;
+    Point _current_point;
+};
+
+}
+
+#endif
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
--- src/2geom/toys/load-svgd.cpp
+++ src/2geom/toys/load-svgd.cpp
@@ -1,80 +1,34 @@
 #include <2geom/d2.h>
 #include <2geom/sbasis.h>
 
-#include <2geom/shape.h>
 #include <2geom/path.h>
 #include <2geom/pathvector.h>
 #include <2geom/svg-path-parser.h>
 
 #include <2geom/toys/path-cairo.h>
 #include <2geom/toys/toy-framework-2.h>
-#include <2geom/transforms.h>
-#include <2geom/sbasis-geometric.h>
+
+#include <2geom/cairo-path-sink.h>
 
 #include <cstdlib>
 
 using namespace Geom;
 
-void cairo_region(cairo_t *cr, Region const &r) {
-    cairo_set_source_rgba(cr, 0, 0, 0, 1); //rand_d(), rand_d(), rand_d(), .75);
-    double d = 5.;
-    if(!r.isFill()) cairo_set_dash(cr, &d, 1, 0);
-    cairo_path(cr, r);
-    cairo_fill(cr);
-    cairo_set_dash(cr, &d, 0, 0);
-}
-
-void cairo_regions(cairo_t *cr, Regions const &p) {
-    srand(0); 
-    for(Regions::const_iterator j = p.begin(); j != p.end(); ++j)
-        cairo_region(cr, *j);
-}
-
-void cairo_shape(cairo_t *cr, Shape const &s) {
-    cairo_regions(cr, s.getContent());
-}
-
-
-void mark_mono(cairo_t *cr, Shape const &a) {
-    for(unsigned j = 0; j < a.size(); j++) {
-        Path p = Path(a[j]);
-        std::vector<double> sp = path_mono_splits(p);
-        for(unsigned i = 0; i < sp.size(); i++) {
-            draw_cross(cr, p.pointAt(sp[i]));
-            cairo_stroke(cr);
-        }
-    }
-}
-
-Shape cleanup(std::vector<Path> const &ps) {
-    Piecewise<D2<SBasis> > pw = paths_to_pw(ps);
-    double area;
-    Point centre;
-    Geom::centroid(pw, centre, area);
-    std::cout << area << "\n";
-    if(area > 1)
-        return sanitize(ps) * Geom::Translate(-centre);
-    else
-        return sanitize(ps);
-}
-
-class BoolOps: public Toy {
+class LoadSVGD: public Toy {
     //Region b;
     //Shape bs;
     PathVector pv;
     PointHandle offset_handle;
     virtual void draw(cairo_t *cr, std::ostringstream *notify, int width, int height, bool save, std::ostringstream *timer_stream) {
-        Geom::Translate t(offset_handle.pos);
-        //Shape bst = bs * t;
-        //Region bt = Region(b * t, b.isFill());
         
         cairo_set_line_width(cr, 1);
         cairo_set_source_rgb(cr, 0,0,0);
-        
-        //cairo_shape(cr, bst);
-        cairo_path(cr, pv*t);
+
+        CairoPathSink sink(cr);
+        sink.pathvector(pv * Translate(offset_handle.pos));
         cairo_stroke(cr);
-        
+
+        // spit out some diagnostic info about the path
         for(unsigned i = 0; i < pv.size(); i++) {
             if(pv[i].size() == 0) {
                 *notify << "naked moveto;";
@@ -95,28 +49,26 @@ class BoolOps: public Toy {
         Toy::draw(cr, notify, width, height, save,timer_stream);
     }
     public:
-    BoolOps () {}
+    LoadSVGD() {}
 
     void first_time(int argc, char** argv) {
         const char *path_b_name="star.svgd";
-        if(argc > 1)
+        if (argc > 1)
             path_b_name = argv[1];
         pv = read_svgd(path_b_name);
         std::cout << pv.size() << "\n";
         std::cout << pv[0].size() << "\n";
         pv *= Translate(-pv[0].initialPoint());
         
-	Rect bounds = *pv[0].boundsExact();
-        std::cout << crossings_among(pv)[0].size() << "\n";
+        Rect bounds = *pv[0].boundsExact();
         handles.push_back(&offset_handle);
         offset_handle.pos = bounds.midpoint() - bounds.corner(0);
-
-        //bs = cleanup(pv);
     }
 };
 
 int main(int argc, char **argv) {
-    init(argc, argv, new BoolOps());
+    LoadSVGD x;
+    init(argc, argv, &x);
     return 0;
 }
 
