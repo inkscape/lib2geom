f0a439d00bc5317eecb44aabc5663011d72e67ff
Johan B. C. Engelen <jbc.engelen@swissonline.ch>
2014-11-09 19:24:47 +0100

    Windows, mingwenv.bat: add Graphviz to the path

--- mingwenv.bat
+++ mingwenv.bat
@@ -6,5 +6,6 @@ set PKG_CONFIG_PATH=c:\devlibs\lib\pkgconfig
 set RAGEL_BIN=c:\ragel
 set GS_BIN=C:\latex\gs\gs9.15\bin
 set PYTHON_PATH=C:\Python27
-set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%RAGEL_BIN%;%PATH%;%GS_BIN%
+set GRAPHVIZ_BIN="C:\Program Files (x86)\Graphviz2.38\bin"
+set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%RAGEL_BIN%;%PATH%;%GS_BIN%;%GRAPHVIZ_BIN%
 set BOOST_DIR=c:\devlibs\include
