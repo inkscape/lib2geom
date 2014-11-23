af90f99415afc3ffba901eb26c13fa69cf8e9841
Johan B. C. Engelen <jbc.engelen@swissonline.ch>
2014-11-23 15:42:57 +0100

    attempt to make mingwenv.bat a bit more structured. (and easier to use)

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,11 +1,18 @@
 @echo Setting environment variables for MinGW build of lib2geom
-set MINGW_BIN=c:\mingw\bin
-set CMAKE_BIN=c:\cmake\bin
-set GTK_BIN=c:\devlibs\bin
-set PKG_CONFIG_PATH=c:\devlibs\lib\pkgconfig
+IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=c:\devlibs
+IF "%MINGW_PATH%"=="" set MINGW_PATH=C:\mingw
+
+
 set RAGEL_BIN=c:\ragel
 set GS_BIN=C:\latex\gs\gs9.15\bin
 set PYTHON_PATH=C:\Python27
 set GRAPHVIZ_BIN="C:\Program Files (x86)\Graphviz2.38\bin"
-set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%RAGEL_BIN%;%PATH%;%GS_BIN%;%GRAPHVIZ_BIN%
-set BOOST_DIR=c:\devlibs\include
+set GS_BIN=C:\latex\gs\gs8.61\bin
+
+set MINGW_BIN=%MINGW_PATH%\bin
+set PKG_CONFIG_PATH=%DEVLIBS_PATH%\lib\pkgconfig
+set CMAKE_PREFIX_PATH=%DEVLIBS_PATH%
+set GTKMM_BASEPATH=%DEVLIBS_PATH%
+set PKG_CONFIG_PATH=%DEVLIBS_PATH%\lib\pkgconfig
+set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTK_BIN%;%RAGEL_BIN%;%PATH%;%GS_BIN%;%GRAPHVIZ_BIN%;%PYTHON_PATH%;%DEVLIBS_PATH%\bin
+set BOOST_DIR=%DEVLIBS_PATH%\include
